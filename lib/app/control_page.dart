eb5c31a82b63f9175c2dc950214ec6f886d6d2d5
Romano Ferreira <arcferreira@id.uff.br>
2020-10-20 00:39:57 -0300

    RF:Firebase e fluxo de auth cliente

---
+++ lib/app/control_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'package:uff_lojinhas/app/sign_in_page/sign_in_page.dart';
+import 'package:uff_lojinhas/services/auth.dart';
+import 'home_page.dart';
+
+/*
+A ControlPage checa o fluxo de autenticacao, identificando mudancas no Stream
+Dependendo do estado do Stream, ela renderiza a pagina de log in (SignInPage) ou
+a pagina de feed do app (HomePage)
+ */
+/*
+Provider e Streambuilder sao classes poderosas e sinceramente o funcionamento delas
+ainda nao eh muito claro pra mim :(
+https://pub.dev/packages/provider
+ */
+
+class ControlPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final auth = Provider.of<AuthBase>(context, listen: false);
+    return StreamBuilder<User>(
+        stream: auth.onAuthStateChanged,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            User user = snapshot.data;
+            if (user == null) {
+              return SignInPage();
+            }
+            return HomePage();
+          }else{
+            return Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        }
+    );
+  }
+
+
+}
---
+++ lib/app/sign_in_page/sign_in_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:uff_lojinhas/services/auth.dart';
+
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  bool _isLoading = false;
+
+  Future<void> _signInAnonymously(BuildContext context) async {
+    try {
+      setState(() => _isLoading = true);
+      final auth = Provider.of<AuthBase>(context, listen: false);
+      await auth.signInAnonymously();
+    } on PlatformException catch (e) {
+      AlertDialog(
+        title: Text("ERROR"),
+        actions: [
+          FlatButton(
+            child: Text("OK"),
+            onPressed: () {},
+          )
+        ],
+      );
+    } finally {
+      setState(() => _isLoading = false);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lojinhas da UFF(signInPage)")
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(30),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(height: 330),
+              RaisedButton(
+                onPressed: () => _signInAnonymously(context),
+                child: Text("Entrar como cliente"),
+              ),
+              SizedBox(height: 16),
+              RaisedButton(
+                onPressed: () {},
+                child: Text("Futuramente vai pra uma pagina de registro :)"),
+              )
+            ]
+        )
+      )
+    );
+  }
+}
