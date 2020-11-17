ec2bfccfe49b79f327d19e1d4308c71c292dc1c2
fauster <lucasfauster@id.uff.br>
2020-11-17 19:02:34 -0300

    show de lojinha ta legal

--- lib/app/screen/show_shop_page.dart
+++ lib/app/screen/show_shop_page.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:uff_lojinhas/app/screen/home_page.dart';
+import 'home_page.dart';
 import '../utils/CardItem.dart';
 import '../model/Shop.dart';
 import '../model/Item.dart';
@@ -79,27 +81,75 @@ class _State extends State<ShowShopPage> {
                   );
                 }
                 return Scaffold(
-                  appBar: AppBar(
-                    title: Text(widget.loja.name),
-                    backgroundColor: Colors.indigo,
-                    /*actions: <Widget>[
-                        FlatButton(
-                            onPressed: () => _goToFeed(context),
-                            child: Text(
-                              "Voltar para o feed",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w400,
-                                  fontSize: 12,
-                                  color: Colors.white),
-                            ))
-                      ]*/
-                  ),
-                  body: new Container(
-                        child: new ListView(
-                          children: createCardList(querySnapshot.documents
-                              .toList()), // Aqui que efetivamente é chamado os cards
-                        ) 
-                      )
+                    appBar: AppBar(
+                        title: Text(widget.loja.name),
+                        backgroundColor: Colors.indigo,
+                        actions: <Widget>[
+                          FlatButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        fullscreenDialog: false,
+                                        builder: (context) => HomePage())
+                                );
+                              },
+                              child: Text(
+                                "Voltar para o feed",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 12,
+                                    color: Colors.white),
+                              ))
+                        ]),
+                    body: new Container(
+                        child: new Column(
+                          children: <Widget>[
+                            new SizedBox(
+                      width: 400.0,
+                      height: 200.0,
+                      child:
+                      new Image.network(widget.loja.urlPhoto, fit: BoxFit.cover)),
+                      new Row(
+                        children: <Widget>[
+                          new Padding(
+                            padding: new EdgeInsets.all(10.0),
+                            child: new Icon(Icons.location_on),
+                          ),
+                          new Padding(
+                            padding: new EdgeInsets.all(10.0),
+                            child: new Text(
+                              "Campus: "+widget.loja.campus,
+                              style: new TextStyle(fontSize: 18.0),
+                            ),
+                          ),
+                          new Padding(
+                            padding: new EdgeInsets.all(10.0),
+                            child: new Text(
+                              "Bloco: " + widget.loja.block,
+                              style: new TextStyle(fontSize: 18.0),
+                            ),
+                          ),
+                          new Padding(
+                            padding: new EdgeInsets.all(10.0),
+                            child: new Text(
+                              "Andar: " + widget.loja.floor,
+                              style: new TextStyle(fontSize: 18.0),
+                            ),
+                          )
+                        ],
+                      ),
+                            new Expanded(
+                              child: new Container(
+                                height: 200.0,
+                                child: new ListView(
+                                  children: createCardList(querySnapshot.documents.toList()),
+                                )
+                              )
+                            ),
+                          ]
+                        ) // Aqui que efetivamente é chamado os cards
+                    )
                 );
               }
           }
